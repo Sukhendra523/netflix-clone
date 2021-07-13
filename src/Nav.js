0346b6574f0c32d1c045342b7940d28a17ccf67e
Sukhendra Rajawat <523sukh@gmail.com>
2021-07-14 02:02:32 +0530

    changing logo

--- src/Nav.js
+++ src/Nav.js
@@ -19,7 +19,7 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        src="netflix-logo-removebg.png"
         alt="Netflix Logo"
       />
 
